28c266f31374f0f8f1f6109fc988613f533aa4ed
heejin99 <wkdgmlwls30@naver.com>
2021-09-30 23:02:52 +0900

    테트리스 수정

--- client/js/board.js
+++ client/js/board.js
@@ -1,54 +1,24 @@
 
-function resize() { 
-    const WINDOW_INNERWIDTH = (window.innerWidth > 660)?660:window.innerWidth;
-    const MAIN_CONTENTS_WIDTH = Math.floor(WINDOW_INNERWIDTH*0.6); 
-    const BLOCK_SIZE = Math.floor(MAIN_CONTENTS_WIDTH/COL_MAIN); 
-    main_ctx.canvas.width = BLOCK_SIZE*COL_MAIN; 
-    main_ctx.canvas.height = BLOCK_SIZE*ROW_MAIN; 
-    main_ctx.scale(BLOCK_SIZE, BLOCK_SIZE); 
-    side_ctx.canvas.width = BLOCK_SIZE*COL_SIDE; 
-    side_ctx.canvas.height = BLOCK_SIZE*ROW_SIDE; 
-    side_ctx.scale(BLOCK_SIZE, BLOCK_SIZE); 
-    const Jua = WINDOW_INNERWIDTH/350; 
-    document.querySelector('#side-cnt').style.fontSize = Jua+'rem'; 
-    setButtonStyle(startB, WINDOW_INNERWIDTH)
-    setButtonStyle(pauseB, WINDOW_INNERWIDTH)
-    setButtonStyle(quitB, WINDOW_INNERWIDTH)
+const mainCanvas = document.getElementById('main-canvas')
+const mainCtx = document.getElementById('2d')
 
-}
+const subCanvas = document.getElementById('sub-canvas')
+const subCtx = document.getElementById('2d')
 
-function setButtonStyle(ele, wid) {
-    ele.style.fontSize = wid/350+'rem'
-    ele.style.paddingTop = wid/1400+'rem'
-    ele.style.paddingBottom = wid/1400+'rem'
-    ele.style.paddingLeft = wid/350+'rem'
-    ele.style.paddingRight = wid/350+'rem'
-}
+mainCtx.fillStyle = "gray"
 
-function initMatrix(row, col) {
-    let matrix = []
-    for (let y = 0; y < row; y++) {
-        matrix.push(new Array(col).fill(0))
+const borderWidth = 5
+const blockSize = 30
+
+function createBorder() {
+    for (let x = 0; x < 12; x++) {
+        for (let y = 0; y < 24; y++) {
+            mainCtx.fillRect(
+                borderWidth * x + blockSize * x,
+                borderWidth * y + blockSize * y,
+                blockSize, blockSize
+            )
+        }
     }
-    return matrix
-}
-function stack(block, matrix) {
-    block.shape.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value > 0) {
-                matrix[y+block.y][x+block.x] = block.shape[y][x]
-            }
-        })
-    })
-}
 
-function drawBoard(matrix, ctx) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value > 0) {
-                ctx.fillStyle='white'
-                ctx.fillRect(x,y,1,1)
-            }
-        })
-    })
 }

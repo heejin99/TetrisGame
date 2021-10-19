eb5d579d3494df3a3e517e749358ac1d14af3951
heejin99 <wkdgmlwls30@naver.com>
2021-10-20 00:33:03 +0900

    heroku db update & mixed content error update

--- server/config/db.js
+++ server/config/db.js
@@ -10,13 +10,30 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 })
 
-db.connect()
+function handleDisconnect() {
+    var intervalId
+    db.connect(function (err) {
+        if (err) {
+            console.log('db error: ',err)
+            setTimeout(handleDisconnect, 2000)
+        }
+    })
+    
+    const preventClosingConnection= ()=>{ 
+        db.query('SELECT 1').catch(e=> console.error(e)); 
+    }; 
+    db.on('error', err => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            return handleDisconnect()
+        } else if (err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR'){
+            intervalId = setInterval(preventClosingConnection, 5* 1000) 
+            clearInterval(intervalId);            
+        } else {
+            throw err
+        }
+    })
+}
 
-db.on('error', err => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        return db.connect()
-    } else {
-        throw err
-    }
-})
+// db.connect()
+handleDisconnect()
 module.exports = db

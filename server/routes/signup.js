c384bb5d972e56aaa1153d647a91c56a59a07996
heejin99 <wkdgmlwls30@naver.com>
2021-10-19 22:32:01 +0900

    heroku signup api mysql update

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -11,19 +11,19 @@ router.get('/', (req, res)=>{
 })
 // 회원가입
 router.post('/', (req, res) => {
-    var param = [req.body.id, req.body.password, req.body.name, req.body.email]
+    var param = [req.body.id, req.body.password, req.body.nickname]
         // 비밀번호 암호화
         bcrypt.hash(param[1], saltRounds, (error, hash) => {
             param[1] = hash
-            db.query('SELECT * FROM user WHERE id=? OR email=?', [req.body.id, req.body.email], (err, data) => {
+            db.query('SELECT * FROM user WHERE id=?', req.body.id, (errr, data) => {
                 if (data.length === 0) {
                     // 아이디와 비밀번호 db에 추가
-                    db.query('INSERT INTO user(`id`,`password`,`name`, `email`) VALUES (?,?,?,?)', param, (err, row) => {
+                    db.query('INSERT INTO user(`id`,`password`,`nickname`) VALUES (?,?,?)', param, (err, row) => {
                         if(err) return res.json({success: false, err})
                         res.redirect('/api')
                     })
                 } else {
-                    res.send('<script>alert("아이디가 존재합니다.");location.href="/api/signup"</script>')
+                    res.send('<script>alert("아이디가 존재합니다.");location.href="/api"</script>')
                 }
             })
         })

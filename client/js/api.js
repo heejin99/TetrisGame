8bf63ee7d3d77964a5b321ffa37c1340de0de1de
heejin99 <wkdgmlwls30@naver.com>
2021-10-19 23:36:33 +0900

    client api BASE_URL update

--- client/js/api.js
+++ client/js/api.js
@@ -1,55 +1,6 @@
-// const BASE_URL = 'http://localhost:3001/'
-// // const axios = require('axios');
-// // import {axios} from '../node_modules/axios/dist/axios'
-// // const axios = require('axios').default;
-// // import axios from 'axios'
 
-// function userPostRequest (body) {
-//   const configObj = {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   }
-//   return window.fetch(BASE_URL + 'users', configObj)
-// }
-
-// function userPatchRequest (game, highScore) {
-//   const body = {
-//     name: user.nickname,
-//     last_game: game,
-//     high_score: highScore
-//   }
-//   const configObj = {
-//     method: 'PATCH',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   }
-//   return window.fetch(BASE_URL + `users/${user.id}`, configObj)
-// }
-
-// function userGetRequest () {
-//   return window.fetch(BASE_URL + `users/${user.id}`)
-// }
-
-// function gamePostRequest (body) {
-//   const configObj = {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   }
-//   return window.fetch(BASE_URL + 'api', configObj)
-// }
-
-const BASE_URL = 'http://localhost:3001/'
+const port = process.env.PORT||3001
+const BASE_URL = 'http://tetris-mobile.herokuapp.com:'+port+'/'
 function gamePostRequest(body) {
     return axios({
         method: 'post',

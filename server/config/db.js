b4b80ea572afd26fa5968c7d3703b6d55302f113
heejin99 <wkdgmlwls30@naver.com>
2021-10-20 01:31:25 +0900

    db connection failed update

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 
 var mysql = require('mysql')
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST, 
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
 function handleDisconnect() {
